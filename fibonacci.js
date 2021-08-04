e4b28e19858ea43e516f1ba35679eb9f15d02ecd
Babin <aqwertybabin@gmail.com>
2021-08-04 12:35:46 +0545

    adds file factorial and fibonacci

---
+++ fibonacci.js
@@ -0,0 +1,18 @@
+function fibonacci(upto){
+    let n1=0, n2 =1, nextTerm;
+
+    console.log('Fibonacci Series: ');
+
+    for(let i=1; i<=upto; i++){
+        console.log(n1);
+        nextTerm=n1+n2;
+        n1=n2;
+        n2=nextTerm;
+    }
+}
+
+fibonacci(10);
+
+
+
+
